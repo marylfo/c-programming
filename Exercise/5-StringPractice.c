66c19ffbcb4bd9ae53f6a6acbad5df93842996ab
Mary Lo <marylo@lscc.edu.hk>
2021-10-25 07:47:48 +0800

    Add exercise 5

---
+++ Exercise/5-StringPractice.c
@@ -0,0 +1,24 @@
+// Include necessary libraries
+#include <stdio.h>
+#include <string.h>
+
+int main() {
+  char x[100] = "supercalifragilisticexpialidocious";
+  char y[20];
+
+  printf("Result:\n");
+
+  // Todo1: Please copy the leftmost 5 characters of x to y
+
+  printf("Leftmost 5 Characters: %s\n", y);
+
+  // Todo2: Please copy the rightmost 7 characters of x to y
+
+  printf("Rightmost 7 Characters: %s", y);
+
+  // Todo3: Please copy the 10 characters from x starting from 6th character (counting from 1st) to y
+
+  printf("10 characters from x starting from 6th character: %s", y);
+
+  return 0;
+}
